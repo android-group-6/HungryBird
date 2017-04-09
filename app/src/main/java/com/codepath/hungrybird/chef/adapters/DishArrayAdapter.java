2263d89cca1ccc12810efde849aad09d376db9f4
Bajaj, Gaurav <gauravb@telenav.com>
2017-04-08 20:57:51 -0700

    Added Offerings page with basic functionality

---
+++ app/src/main/java/com/codepath/hungrybird/chef/adapters/DishArrayAdapter.java
@@ -0,0 +1,131 @@
+package com.codepath.hungrybird.chef.adapters;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.codepath.hungrybird.R;
+import com.codepath.hungrybird.databinding.ChefOfferingsDishesListItemBinding;
+import com.codepath.hungrybird.databinding.ProgressItemBinding;
+import com.codepath.hungrybird.model.Dish;
+
+import java.util.ArrayList;
+
+/**
+ * Created by gauravb on 3/15/17.
+ */
+
+public class DishArrayAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String TAG = DishArrayAdapter.class.getSimpleName();
+    ArrayList<Dish> dishArrayList;
+    Context context;
+    private final int VIEW_ITEM = 1;
+    private final int VIEW_PROG = 0;
+
+    public DishArrayAdapter(Context context, ArrayList<Dish> dishArrayList) {
+        this.dishArrayList = dishArrayList;
+        this.context = context;
+        if (dishArrayList == null) {
+            throw new NullPointerException("Article ArrayList Can't Be Null or Empty");
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        RecyclerView.ViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        if (viewType == VIEW_ITEM) {
+            // Inflate the custom layout
+            ChefOfferingsDishesListItemBinding binding =
+                    DataBindingUtil.inflate(inflater, R.layout.chef_offerings_dishes_list_item, parent, false);
+
+            // Return a new holder instance
+            viewHolder = new ViewHolder(binding);
+        } else {
+            // Inflate the custom layout
+            ProgressItemBinding binding =
+                    DataBindingUtil.inflate(inflater, R.layout.progress_item, parent, false);
+            viewHolder = new ProgressViewHolder(binding.getRoot());
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        //Get the data iterm from the current position
+        if (viewHolder instanceof ViewHolder) {
+            ViewHolder holder = (ViewHolder) viewHolder;
+            final Dish dish = dishArrayList.get(position);
+            holder.binding.chefOfferingListItemDishNameTv.setText(dish.getDishName());
+            holder.binding.chefOfferingDishListItemServingSizeValueTv.setText("" + dish.getServingSize());
+            holder.binding.chefOfferingDishListItemPriceTv.setText("$" + dish.getPrice());
+        } else {
+            ((ProgressViewHolder) viewHolder).progressBar.setIndeterminate(true);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return dishArrayList.get(position) != null ? VIEW_ITEM : VIEW_PROG;
+    }
+
+    @Override
+    public int getItemCount() {
+        return dishArrayList.size();
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        ChefOfferingsDishesListItemBinding binding;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(ChefOfferingsDishesListItemBinding binding) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(binding.getRoot());
+            this.binding = binding;
+
+            itemView.setOnClickListener(view -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) { // Check if an item was deleted, but the user clicked it before the UI removed it
+                    Dish dish = dishArrayList.get(position);
+                    dishSelected(dish);
+                    // We can access the data within the views
+
+                    /* No longer needed this code As we are Using Chrome Tab now
+
+                    Intent i = new Intent(context, ArticleActivity.class);
+                    i.putExtra("article", Parcels.wrap(article));
+                    context.startActivity(i);
+                    Log.d(TAG, "Message " + article + " clicked");
+
+                    */
+                }
+            });
+        }
+    }
+
+    public static class ProgressViewHolder extends RecyclerView.ViewHolder {
+        public ProgressBar progressBar;
+
+        public ProgressViewHolder(View v) {
+            super(v);
+            progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        }
+    }
+
+    private void dishSelected(Dish dish) {
+        Toast.makeText(context, "" + dish.getDishName(), Toast.LENGTH_SHORT).show();
+
+    }
+}
