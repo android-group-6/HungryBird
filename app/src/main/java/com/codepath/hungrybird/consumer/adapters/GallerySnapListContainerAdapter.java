fe05aeb5d29f23fa415869003d4f49f75cc3a389
Dhwani Shah <dhwani@besomebody.com>
2017-04-24 18:51:09 -0400

    Made UI changes to chef add edit page

--- app/src/main/java/com/codepath/hungrybird/consumer/adapters/GallerySnapListContainerAdapter.java
+++ app/src/main/java/com/codepath/hungrybird/consumer/adapters/GallerySnapListContainerAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.hungrybird.consumer.adapters;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
@@ -62,10 +63,10 @@ public class GallerySnapListContainerAdapter extends RecyclerView.Adapter<Galler
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         DishList dishList = mDishes.get(position);
-//        String color = colorChooser.getColor(dishList.getText());
-//        if (color != null) {
-//            holder.snapTextView.setBackgroundColor(Color.parseColor(color));
-//        }
+        String color = colorChooser.getColor(dishList.getText());
+        if (color != null) {
+            holder.snapTextView.setTextColor(Color.parseColor(color));
+        }
         StringBuilder sb = new StringBuilder();
         sb.append(dishList.getText());
         sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
