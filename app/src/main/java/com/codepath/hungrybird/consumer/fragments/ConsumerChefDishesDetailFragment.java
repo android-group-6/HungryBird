d6ba1f7f1b40eadfa1725cb8682d3af3969e85dd
Bajaj, Gaurav <gaurav.bajaj.83@gmail.com>
2017-05-03 22:06:49 -0700

    Added add to cart animation

--- app/src/main/java/com/codepath/hungrybird/consumer/fragments/ConsumerChefDishesDetailFragment.java
+++ app/src/main/java/com/codepath/hungrybird/consumer/fragments/ConsumerChefDishesDetailFragment.java
@@ -1,5 +1,8 @@
 package com.codepath.hungrybird.consumer.fragments;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
@@ -16,6 +19,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -119,7 +124,7 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
         binding.chefMyoffersingsLv.addItemDecoration(dividerItemDecoration);
 
         adapter.setViewBinder((holder, item, position) -> {
-            Log.d(TAG, "onCreateView: setViewBinder " +orderDishRelationResponse);
+            Log.d(TAG, "onCreateView: setViewBinder " + orderDishRelationResponse);
             ChefOfferingsDishesListItemBinding binding = (ChefOfferingsDishesListItemBinding) (holder.binding);
             final Dish dish = dishesArrayList.get(position);
             binding.chefOfferingListItemDishNameTv.setText(dish.getTitle());
@@ -167,13 +172,56 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
                         if (e == null) {
                             Log.d(TAG, "onCreateView: added Successfully " + position);
                             orderDishRelationResponse.map.put(dish.getObjectId(), odr);
-                            adapter.notifyItemChanged(position);
+                            binding.rowCartInclude.cart.animate().alpha(0f).scaleX(0)
+                                    .setInterpolator(new AccelerateInterpolator())
+                                    .scaleY(1).setDuration(200).setListener(new Animator.AnimatorListener() {
+                                @Override
+                                public void onAnimationStart(Animator animation) {
+                                }
+
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    binding.rowCartInclude.cart.setVisibility(View.GONE);
+                                }
+
+                                @Override
+                                public void onAnimationCancel(Animator animation) {
+                                }
+
+                                @Override
+                                public void onAnimationRepeat(Animator animation) {
+                                }
+                            }).start();
+
+                            binding.increaseDecreaseInclude.increaseDecrease.
+                                    animate().alpha(1f).scaleX(1).scaleY(1).setStartDelay(75)
+                                    .setInterpolator(new DecelerateInterpolator())
+                                    .setDuration(300).setListener(new Animator.AnimatorListener() {
+                                @Override
+                                public void onAnimationStart(Animator animation) {
+                                }
+
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    binding.increaseDecreaseInclude.increaseDecrease.setVisibility(View.VISIBLE);
+                                }
+
+                                @Override
+                                public void onAnimationCancel(Animator animation) {
+                                }
+
+                                @Override
+                                public void onAnimationRepeat(Animator animation) {
+                                }
+                            }).start();
+
                         }
                     }
                 });
 
             });
             binding.increaseDecreaseInclude.increase.setOnClickListener(v -> {
+                Log.d(TAG, "onCreateView: " + "increase count");
                 OrderDishRelation odr = orderDishRelationResponse.map.get(dish.getObjectId());
                 if (odr != null) {
                     odr.setQuantity(odr.getQuantity() + 1);
@@ -181,7 +229,13 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
                         @Override
                         public void done(ParseException e) {
                             if (e == null) {
-                                adapter.notifyItemChanged(position);
+                                Log.d(TAG, "done: show items count");
+                                ValueAnimator fadeAnim = ObjectAnimator.ofFloat(binding.chefOfferingDishListItemCountTv, "alpha", 0f, 1f);
+                                fadeAnim.setInterpolator(new DecelerateInterpolator());
+                                fadeAnim.setDuration(250);
+                                fadeAnim.start();
+                                binding.chefOfferingDishListItemCountTv.setVisibility(View.VISIBLE);
+                                binding.chefOfferingDishListItemCountTv.setText("x " + odr.getQuantity());
                             }
                         }
                     });
@@ -196,7 +250,39 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
                             @Override
                             public void done(ParseException e) {
                                 if (e == null) {
-                                    adapter.notifyItemChanged(position);
+                                    if (odr.getQuantity() > 1) {
+                                        ValueAnimator fadeAnim = ObjectAnimator.ofFloat(binding.chefOfferingDishListItemCountTv, "alpha", 0f, 1f);
+                                        fadeAnim.setInterpolator(new DecelerateInterpolator());
+                                        fadeAnim.setDuration(250);
+                                        fadeAnim.start();
+                                        binding.chefOfferingDishListItemCountTv.setText("x " + odr.getQuantity());
+                                    } else if (odr.getQuantity() == 1) {
+                                        ValueAnimator fadeAnim = ObjectAnimator.ofFloat(binding.chefOfferingDishListItemCountTv, "alpha", 1f, 0f);
+                                        fadeAnim.setInterpolator(new DecelerateInterpolator());
+                                        fadeAnim.addListener(new Animator.AnimatorListener() {
+                                            @Override
+                                            public void onAnimationStart(Animator animation) {
+
+                                            }
+
+                                            @Override
+                                            public void onAnimationEnd(Animator animation) {
+                                                binding.chefOfferingDishListItemCountTv.setVisibility(View.GONE);
+                                            }
+
+                                            @Override
+                                            public void onAnimationCancel(Animator animation) {
+
+                                            }
+
+                                            @Override
+                                            public void onAnimationRepeat(Animator animation) {
+
+                                            }
+                                        });
+                                        fadeAnim.setDuration(250);
+                                        fadeAnim.start();
+                                    }
                                 }
                             }
                         });
@@ -206,7 +292,80 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
                             @Override
                             public void onSuccess(OrderDishRelation orderDishRelation) {
                                 orderDishRelationResponse.map.remove(dish.getObjectId());
-                                adapter.notifyItemChanged(position);
+                                ValueAnimator fadeAnim = ObjectAnimator.ofFloat(binding.chefOfferingDishListItemCountTv, "alpha", 1f, 0f);
+                                fadeAnim.setInterpolator(new DecelerateInterpolator());
+                                fadeAnim.setDuration(200);
+                                fadeAnim.addListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        binding.chefOfferingDishListItemCountTv.setVisibility(View.GONE);
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animation) {
+
+                                    }
+                                });
+
+                                fadeAnim.start();
+                                binding.increaseDecreaseInclude.increaseDecrease.animate().alpha(0f).scaleX(0)
+                                        .setInterpolator(new AccelerateInterpolator())
+                                        .scaleY(1).setDuration(200).setListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        binding.increaseDecreaseInclude.increaseDecrease.setVisibility(View.GONE);
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animation) {
+
+                                    }
+                                }).start();
+
+                                binding.rowCartInclude.cart.
+                                        animate().alpha(1f).scaleX(1).setStartDelay(75)
+                                        .setInterpolator(new DecelerateInterpolator())
+                                        .scaleY(1).setDuration(300).setListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        binding.rowCartInclude.cart.setVisibility(View.VISIBLE);
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animation) {
+
+                                    }
+                                }).start();
                             }
 
                             @Override
@@ -215,6 +374,7 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
                             }
                         });
                     }
+
                 }
             });
             holder.getBaseView().setOnClickListener(new View.OnClickListener() {
@@ -334,10 +494,13 @@ public class ConsumerChefDishesDetailFragment extends Fragment implements DishAr
                 ParseFile chefProfilePic = user.getProfileImage();
                 if (chefProfilePic != null && chefProfilePic.getUrl() != null) {
                     String imgUrl = chefProfilePic.getUrl();
-                    Glide.with(getContext())
-                            .load(imgUrl)
-                            .bitmapTransform(new CropCircleTransformation(getContext()))
-                            .into(binding.chefProfilePicIv);
+                    Context context = getContext();
+                    if (context != null) {
+                        Glide.with(context)
+                                .load(imgUrl)
+                                .bitmapTransform(new CropCircleTransformation(context))
+                                .into(binding.chefProfilePicIv);
+                    }
                 }
                 binding.chefNameTv.setText(user.getUsername());
             }
